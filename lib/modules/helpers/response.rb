027a2dabc6900aede81a221f3e314b33aa447054
Wesley Day <wesleyday@hotmail.com>
2025-04-19 13:01:00 -0500

    Update response.rb

--- lib/modules/helpers/response.rb
+++ lib/modules/helpers/response.rb
@@ -2,14 +2,6 @@
 
 # Response helpers
 module Bot
-  def self.log_command(event)
-    Bot.log "#{event.author.username}: #{event.content}"
-    event.message.delete unless event.message.channel.pm?
-  rescue StandardError => e
-    Bot.log 'Error while logging response.'
-    Bot.log_exception(e)
-  end
-
   def self.message_response(event, msg)
     Bot.log_command(event)
     event.channel.split_send(msg)
